23e9a8fbbe1231f13ad768aab4fd83e16ad12ccc
zhao_nan <zhaonan@litsoft.com.cn>
2019-01-11 17:03:42 +0800

    merge 1703

---
+++ app/js/home/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="home">
+        <h1>home</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    }
+};
+</script>
+
+<style>
+.home {
+    color: red;
+    font-size: 80px;
+}
+</style>
